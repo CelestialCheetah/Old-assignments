afe8339360a75bf126b36bb898aa467801fc1296
Ivan Knowles <53583290+CelestialCheetah@users.noreply.github.com>
2021-02-26 22:28:08 -0500

    Add files via upload

---
+++ hw11c++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Pizza{
+
+Public:
+    enum Size{small, medium, large};
+    double amountOfToppings();
+    double computePrice();
+    void outputDescription();
+
+
+
+};
+
+int main()
+{
+
+}
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int findMax(int questions, int guessed, int actual);
+int main()
+{
+    int questions = 0;
+    int guessed = 0;
+    int actual = 0;
+
+    if( questions < = 10)
+        questions++;
+
+    if ( guessed < =10 )
+        guessed++;
+
+    if ( actual <= 10)
+        actual++;
+
+    cout<<"hi"<<end;;
+    return 0;
+}
