caf6d681604b15b2e7f237d36821c2bbbb1f683f
Ivan Knowles <53583290+CelestialCheetah@users.noreply.github.com>
2021-02-26 22:35:03 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,22 +2,8 @@
 
 using namespace std;
 
-int findMax(int questions, int guessed, int actual);
 int main()
 {
-    int questions = 0;
-    int guessed = 0;
-    int actual = 0;
-
-    if( questions < = 10)
-        questions++;
-
-    if ( guessed < =10 )
-        guessed++;
-
-    if ( actual <= 10)
-        actual++;
-
-    cout<<"hi"<<end;;
+    cout << "Hello world!" << endl;
     return 0;
 }
